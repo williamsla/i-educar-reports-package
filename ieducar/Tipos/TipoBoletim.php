784828847f47d94d74825504dfd8b9fe8bd1c7bf
williams <williamsalcantara@gmail.com>
2025-09-02 03:31:47 -0300

    boletim com recuperação semestral

--- ieducar/Tipos/TipoBoletim.php
+++ ieducar/Tipos/TipoBoletim.php
@@ -7,12 +7,14 @@ class Portabilis_Model_Report_TipoBoletim extends CoreExt_Enum
     const CONCEPTUAL_LANDSCAPE = 3;
     const PARECER_DESCRITIVO_COMPONENTE = 9;
     const PARECER_DESCRITIVO_GERAL = 10;
+    const NUMERIC_WITH_SEMESTER_RECOVERY = 11;
 
     protected $_data = [
         self::NUMERIC => 'Boletim numérico',
         self::CONCEPTUAL => 'Boletim conceitual',
         self::PARECER_DESCRITIVO_COMPONENTE => 'Parecer descritivo por componente',
-        self::PARECER_DESCRITIVO_GERAL => 'Parecer descritivo geral'
+        self::PARECER_DESCRITIVO_GERAL => 'Parecer descritivo geral',
+        self::NUMERIC_WITH_SEMESTER_RECOVERY => 'Boletim numérico com recuperação semestral',
     ];
 
     protected $_reports = [
@@ -21,6 +23,7 @@ class Portabilis_Model_Report_TipoBoletim extends CoreExt_Enum
         self::CONCEPTUAL_LANDSCAPE => 'conceptual-landscape-report-card',
         self::PARECER_DESCRITIVO_COMPONENTE => 'descriptive-opinion-report-card',
         self::PARECER_DESCRITIVO_GERAL => 'general-opinion-report-card',
+        self::NUMERIC_WITH_SEMESTER_RECOVERY => 'report-card-with-semester-recovery',
     ];
 
     public function getReports()
